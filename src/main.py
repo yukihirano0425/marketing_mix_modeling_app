a1292ca46f60832a6e8c22fe547ec72bce46b44e
yukihirano0425 <yuki.hirano@uzabase.com>
2023-09-21 09:22:18 +0900

    コスト推移を可視化する関数の実装

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
 pass
+
+target = "sales"
+sales_channel = ["営業チャネルA", "営業チャネルB", "営業チャネルC", "営業チャネルD", "営業チャネルE"]
+non_sales_channel = ["非営業チャネルA", "非営業チャネルB", "非営業チャネルC", "非営業チャネルD", "非営業チャネルE"]
+whole_cost = sales_channel + non_sales_channel
+features = sales_channel + non_sales_channel
---
+++ src/utils/plot_cost_transition.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def plot_cost_transition(df: pd.DataFrame, features: list[str]) -> None:
+    """
+    指定した営業チャネルのコスト推移をグラフ表示する
+    args:
+      df: 日別のコストデータの入ったデータフレーム
+      features: 表示したい営業チャネル
+        ex: 複数チャネルを表示する場合) ["sales_channel_A", "sales_channel_B", "sales_channel_C"]
+        ex: 単一チャネルを表示する場合) ["sales_channel_A"]
+    """
+
+    plt.figure(figsize=(17, 7))
+    plt.plot(df["date"], df[features], label=features)
+
+    plt.title(f"{features[0]}")
+    plt.xticks(rotation=90)
+    plt.ylabel("コスト(円)")
+    plt.legend(fontsize=14, loc="upper left")
+    plt.ticklabel_format(style="plain", axis="y")
+
+    plt.show()
+    plt.tight_layout()
